236d72ce4ffa1be1b01ba0e3cdb5d2ca1151a118
Tasio Méndez <tasio.mendez.ayerbe@alumnos.upm.es>
2018-01-01 19:21:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Despliegue de un sistema CRM escalable
+
+El objetivo es la creación de un escenario completo de despliegue de una aplicación fiable y escalable que integre los diversos contenidos impartidos en la asignatura.
+
+La aplicación CRM la podemos encontrar en el siguiente [enlace](https://github.com/CORE-UPM/CRM_2017).
+
+## Descripción
+
+En el proyecto se utilizarán los elementos típicos de las arquitecturas actuales: firewall, balanceador de carga, servidores font-end corriendo la aplicación, bases de datos y servidores de almacenamiento, tal y como aparece representado en la siguiente figura.
+
+![stage](stage.png)
+
+La aplicación CRM se configurará para que utilice una base de datos PostgreSQL, que correrá en el servidor de bases de datos, y para que almacene las imágenes en el cluster de almacenamiento que se creará utilizando el sistema de ficheros distribuido GlusterFS. El balanceador de carga se ocupará de distribuir la carga entre los tres servidores que soportan la aplicación CRM (S1, S2 y S3) y el cortafuegos de entrada, basado en el software de Linux FirewallBuilder, se ocupará de filtrar todo el tráfico proveniente de Internet y dejar pasar únicamente el destinado a la aplicación.
+
+La arquitectura debe garantizar la escalabilidad de la aplicación, permitiendo ampliar fácilmente el número de servidores dedicados según crezca el número de usuarios. Por ello se parte de un sistema con un número determinado de servidores, pero se prevé añadir servidores (reales o virtuales) según crezca la demanda del servicio.
+
+* El servicio CRM debe estar accesible a los clientes en el puerto estándar de web (80).
+* El balanceador debe balancear la carga entre todos los servidores utilizando un algoritmo round-robin.
+* El cluster de almacenamiento, que se utilizará para almacenar las imágenes utilizadas por el CRM se debe configurar de forma que utilice GlusterFS y que replique la información entre los tres servidores (nas1, nas2 y nas3).
+* La información manejad por el CRM se debe almacenar en el servidor de bases de datos, utilizando PostgreSQL (recomendado) u otro gestor de bases de datos soportado por el CRM. En cualquier caso, debe utilizarse una base de datos externa desplegada en el servidor destinado a tales efectos. Por lo tanto, no puede utilizarse SQLite.
+* El firewall debe permitir únicamente el acceso mediante ping y al puerto 80 de TCP de la dirección balanceador de tráfico. Cualquier otro tráfico debe de estar prohibido.
+
+## Puesta en marcha del escenario
+
+1. **Si utiliza ordenador propio con VirtualBox**
+
+  * Descargue la máquina virtual a su ordenador desde este [enlace](http://idefix.dit.upm.es/cdps/CDPS2017-v1.ova), e impórtela a VirtualBox y arranquela.
+
+  * Accede a un terminal de la máquina virtual y descargue y descomprima el escenario.
+    ```shell
+    wget http://idefix.dit.upm.es/cdps/pfinal/pfinal.tgz
+    sudo vnx --unpack pfinal.tgz && cd pfinal
+    bin/prepare-pfinal-vm
+    ```
+
+2. **Si utiliza ordenador propio con Linux y VNX**, accede a un terminal del PC y descargue el escenario y descomprímalo mediante:
+
+  ```shell
+  wget http://idefix.dit.upm.es/cdps/pfinal/pfinal.tgz
+  sudo vnx --unpack pfinal.tgz && cd pfinal
+  bin/prepare-pfinal-vm
+  ```
+
+3. **Si utiliza el laboratorio**, entre en su cuenta, acceda a un terminal, descargue el escenario y descomprímalo.
+
+  ```shell
+  wget http://idefix.dit.upm.es/cdps/pfinal/pfinal.tgz
+  sudo vnx --unpack pfinal.tgz && cd pfinal
+  bin/prepare-pfinal-labo
+  ```
+
+  > Por restricciones de espacio en el laboratorio es necesario trabajar en el directorio /mnt/tmp.
+
+Finalmente, para todas las opciones, copie el script de configuración en la carpeta `pfinal` que se ha creado al montar el escenario y ejecute el script como:
+
+```python config.py```
+
+## Autores
+
+Esta práctica ha sido realizada por [Tasio Méndez Ayerbe](https://github.com/tasiomendez/).
---
+++ config.py
@@ -0,0 +1,188 @@
+#! /usr/bin/python
+
+'''
+Despliegue de un sistema CRM escalable
+======================================
+
+Script de configuracion automatica para una implementacion de la 
+arquitectura completa de un sistema CRM escalable y fiable basado
+en el proyecto CRM utilizado en otras asignaturas. Se utilizaran los 
+elementos tipicos de las arquitecturas actuales: firewall, balanceador 
+de carga, servidores front-end corriendo la aplicacion, bases de datos 
+y servidores de almacenamiento.
+'''
+
+import argparse
+import logging
+import os
+from subprocess import call
+from contextlib import contextmanager
+from time import time, sleep
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(name)s: %(message)s')
+logger = logging.getLogger('CDPS')
+
+N_SERVERS_DEFAULT = 3
+N_SERVERS_CLUSTER_DEFAULT = 3
+
+DATABASE_URL = 'postgres://crm:xxxx@10.1.4.31:5432/crm'
+
+devnull = open(os.devnull, 'w')
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Configuracion automatica del despliegue de un sistema CRM escalable')
+
+	parser.add_argument('FILE', help='VNX File para crear el escenario')
+	parser.add_argument('-n', '--no-console', help='Arrancar el escenario sin mostrar las consolas', action='store_false')
+
+	args = parser.parse_args()
+
+	with timer('Escenario desplegado'):
+		create(args.FILE, args.no_console)
+		bbdd()
+		storage()
+		crm()
+		load_balancer()
+
+	print('')
+
+
+def create(file, console):
+	logger.info('Creando escenario...')
+	if console:
+		call('sudo vnx -f {file} --create'.format(file=file), shell=True, stdout=devnull)
+	else:
+		call('sudo vnx -f {file} --create --no-console'.format(file=file), shell=True, stdout=devnull)
+	logger.info('Escenario creado.')
+
+
+def firewall():
+	'''
+	Configuracion del cortafuegos. Se debe permitir unicamente el 
+	acceso mediante ping y al puerto 80 de TCP de la direccion 
+	balanceador de trafico. 
+	Cualquier otro trafico debe de estar prohibido.
+	'''
+	logger.info('Configurando firewall...')
+	call('sudo lxc-attach --clear-env -n fw -- < ./fw.fw', shell=True)
+	logger.info('Firewall configurado.')
+
+
+def bbdd():
+	'''
+	Configuracion de la base de datos PostgreSQL que utilizaran los 
+	servidores.
+	'''
+	logger.info('Configuracion de PostgreSQL')
+	name = 'bbdd'
+	lxc = 'sudo lxc-attach --clear-env -n {name}'.format(name=name)
+
+	logger.info('\t Configurando la base de datos...')
+
+	cmd_line = [
+		'{lxc} -- bash -c "echo \\\"listen_addresses=\'10.1.4.31\'\\\" >> /etc/postgresql/9.6/main/postgresql.conf"'.format(lxc=lxc),
+		'{lxc} -- bash -c "echo \\\"host all all 10.1.4.0/24 trust\\\" >> /etc/postgresql/9.6/main/pg_hba.conf"'.format(lxc=lxc),
+		'{lxc} -- bash -c "echo \\\"CREATE USER crm with password \'xxxx\';\\\" | sudo -u postgres psql"'.format(lxc=lxc),
+		'{lxc} -- bash -c "echo \\\"CREATE DATABASE crm;\\\" | sudo -u postgres psql"'.format(lxc=lxc),
+		'{lxc} -- bash -c "echo \\\"GRANT ALL PRIVILEGES ON DATABASE crm to crm;\\\" | sudo -u postgres psql"'.format(lxc=lxc),
+		'{lxc} -- bash -c "systemctl restart postgresql"'.format(lxc=lxc)
+	]
+
+	for i in range(len(cmd_line)):
+		call(cmd_line[i], shell=True)
+
+	logger.info('PostgreSQL configurado.')
+
+
+def storage():
+	'''
+	Configuracion del sistema de ficheros GlusterFS que replica la 
+	informacion entre los tres servidores nas.
+	'''
+
+	logger.info('Configuracion del GlusterFS')
+
+	# Creacion del cluster (desde nas1)
+	lxc = 'sudo lxc-attach --clear-env -n {name}'.format(name='nas1')
+
+	cmd_line = 'gluster volume create nas replica {n}'.format(n=N_SERVERS_CLUSTER_DEFAULT)
+
+	for i in range(1, N_SERVERS_CLUSTER_DEFAULT + 1):
+		call('{lxc} -- bash -c "gluster peer probe 10.1.4.2{n}"'.format(lxc=lxc, n=str(i)), shell=True)
+
+		cmd_line += ' 10.1.4.2{n}:/nas'.format(n=str(i))
+		sleep(0.5)
+
+	call('{lxc} -- bash -c "{cmd_line} force"'.format(lxc=lxc, cmd_line=cmd_line), shell=True)
+	call('{lxc} -- bash -c "gluster volume start nas"'.format(lxc=lxc), shell=True)
+
+	cmd_line = 'gluster volume set nas network.ping-timeout 5'
+	for i in range(1, N_SERVERS_CLUSTER_DEFAULT + 1):
+		call('sudo lxc-attach --clear-env -n nas{n} -- bash -c "{cmd_line}"'.format(n=str(i), cmd_line=cmd_line), shell=True)
+
+	# Configuracion del cluster desde los servidores web
+	for i in range(1, N_SERVERS_DEFAULT + 1):
+		lxc = 'sudo lxc-attach --clear-env -n s{n}'.format(n=str(i))
+		call('{lxc} -- bash -c "mkdir /mnt/nas"'.format(lxc=lxc), shell=True)
+		call('{lxc} -- bash -c "mount -t glusterfs 10.1.4.21:/nas /mnt/nas"'.format(lxc=lxc), shell=True)
+
+	logger.info('GlusterFS configurado.')
+
+
+def crm():
+	'''
+	Instalacion y configuracion  de la aplicacion CRM en los servidores.
+
+	Disponible en https://github.com/CORE-UPM/CRM_2017
+	'''
+	logger.info('Instalacion del CRM')
+
+	for i in range(1, N_SERVERS_DEFAULT + 1):
+		lxc = 'sudo lxc-attach --clear-env -n s{n} --set-var DATABASE_URL={url}'.format(n=str(i), url=DATABASE_URL)
+
+		call('{lxc} -- bash -c "cd /root; git clone https://github.com/CORE-UPM/CRM_2017.git"'.format(lxc=lxc), shell=True, stdout=devnull)
+		call('{lxc} -- bash -c "cd /root/CRM_2017; npm install; npm install forever"'.format(lxc=lxc), shell=True, stdout=devnull, stderr=devnull)
+
+		# Creamos la base de datos solo en un servidor
+		if (i == 1):
+			call('{lxc} -- bash -c "cd /root/CRM_2017; npm run-script migrate_local; npm run-script seed_local"'.format(lxc=lxc), shell=True)
+
+		logger.info('\t CRM instalado en s{}.'.format(str(i)))
+
+		# Redirigimos las imagenes al cluster
+		if (i == 1):
+			call('{lxc} -- bash -c "mkdir /mnt/nas/uploads"'.format(lxc=lxc), shell=True)
+		call('{lxc} -- bash -c "ln -s /mnt/nas/uploads /root/CRM_2017/public/uploads"'.format(lxc=lxc), shell=True)
+		
+		# Arrancamos la aplicacion
+		call('{lxc} -- bash -c "cd /root/CRM_2017; ./node_modules/forever/bin/forever start ./bin/www"'.format(lxc=lxc), shell=True)
+
+	logger.info('CRM instalado satisfactoriamente.')
+
+
+def load_balancer():
+	'''
+	Configuracion del balanceador de trafico para que utilice el 
+	algoritmo round-robin.
+	'''
+	cmd_line = 'sudo lxc-attach --clear-env -n lb -- xr --server tcp:0:80 -dr'
+
+	for i in range(1, N_SERVERS_DEFAULT + 1):
+		cmd_line += ' --backend 10.1.3.1{n}:3000'.format(n=str(i))
+
+	cmd_line += ' --web-interface 0:8001 &'
+
+	call(cmd_line, shell=True)
+
+
+@contextmanager
+def timer(name='task', function=logger.info):
+    start = time()
+    yield start
+    end = time()
+    function('{} en {} segundos'.format(name, end - start))	
+
+
+if __name__ == '__main__':
+	main()
